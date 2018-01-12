29394a6bead0f3400ea7148656826148ed7178ce
HyeongSik Kim <yy20716@gmail.com>
2018-01-12 14:41:00 -0800

    revise the codes so that the program can accept directory as input

--- src/main/java/org/monarchinitiative/resourceuristatuschecker/ResourceURIStatusChecker.java
+++ src/main/java/org/monarchinitiative/resourceuristatuschecker/ResourceURIStatusChecker.java
@@ -3,10 +3,13 @@ package org.monarchinitiative.resourceuristatuschecker;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.util.List;
 import java.util.Random;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.validator.routines.UrlValidator;
 import org.apache.jena.ext.com.google.common.collect.Sets;
 import org.apache.jena.query.Query;
 import org.apache.jena.query.QueryExecution;
@@ -23,16 +26,31 @@ import org.jsoup.nodes.Document;
 
 public class ResourceURIStatusChecker {
 	private static final Logger logger = Logger.getLogger(ResourceURIStatusChecker.class.getName());
-	private static final String outputFilename = "output.log";
-	private String checkwordArr[] = {"warning","error","fatal","invalid"};
+	private String checkwordArr[] = {"warn","error","fatal","invalid"};
 	private Set<RDFNode> nodeTrailSet = Sets.newHashSet();
 	private Random rand = new Random();
+	private String[] schemes = {"http","https"};
+	private UrlValidator urlValidator = new UrlValidator(schemes);
 
 	public void run(String inputFilePath) {
+		File inputFile = new File(inputFilePath);
+		if (inputFile.isDirectory())
+			runOverDir(inputFile);
+		else
+			runOverFile(inputFile);
+	}
+
+	public void runOverDir(File inputDir) {
+		String[] extensions = new String[] { "ttl", "owl" };
+		List<File> files = (List<File>) FileUtils.listFiles(inputDir, extensions, true);
+		for (File file : files)
+			runOverFile(file);
+	}
+
+	public void runOverFile(File inputFile) {
 		BufferedWriter bw = null;
 		FileWriter fw = null;
 
-		File inputFile = new File(inputFilePath);
 		String inputFilename = inputFile.getName();
 		int pointPos = inputFilename.lastIndexOf(".");
 		File file = new File(inputFilename.substring(0, pointPos) + "_output.log");
@@ -42,37 +60,37 @@ public class ResourceURIStatusChecker {
 			fw = new FileWriter(file.getAbsoluteFile(), true);
 			bw = new BufferedWriter(fw);
 
-			Model onetimeModel = FileManager.get().loadModel(inputFilePath);
+			Model onetimeModel = FileManager.get().loadModel(inputFile.toString());
 			String queryString = "SELECT * WHERE { ?s ?p ?o }";
 			Query query = QueryFactory.create(queryString);
 			QueryExecution qexec = QueryExecutionFactory.create(query, onetimeModel);
 			ResultSet results = qexec.execSelect() ;
-
-			long count = 1;
 			long modelSize = onetimeModel.size();
 
 			logger.info("Model size: " + modelSize);
+			logger.info("Validating URIs of Nodes ....");
 			for ( ; results.hasNext() ; ) {
-				logger.info("(" + count + "/" + modelSize + ")");
-
 				QuerySolution soln = results.nextSolution();
-				checkNodeURI(soln.get("s"), bw);
-				checkNodeURI(soln.get("p"), bw);
-				checkNodeURI(soln.get("o"), bw);
-
-				count += 1;
+				validateNodeURI(soln.get("s"), bw);
+				validateNodeURI(soln.get("p"), bw);
+				validateNodeURI(soln.get("o"), bw);
+			}
+			
+			long nodeSetSize = nodeTrailSet.size();
+			logger.info("Checking the status of URIs of Nodes ....");
+			logger.info("#Nodes: " + nodeSetSize);
+			for (RDFNode node : nodeTrailSet) {
+				checkNodeURIStatus(node, bw);
 			}
 		} catch (Exception e1) {
 			logger.error(e1.getMessage(), e1);
 		} finally {
 			try {
 				if (bw != null) {
-					bw.flush();
 					bw.close();
 				}
 
 				if (fw != null) {
-					fw.flush();
 					fw.close();
 				}
 			} catch (Exception e2) {
@@ -81,14 +99,30 @@ public class ResourceURIStatusChecker {
 		}
 	}
 
-	public void checkNodeURI(RDFNode node, BufferedWriter bw) {
-		if (nodeTrailSet.contains(node)) return;
-		if (node.isLiteral()) return;
-		if (node.isAnon()) return;
+	public void validateNodeURI(RDFNode node, BufferedWriter bw) {
+		try {
+			if (nodeTrailSet.contains(node)) return;
+			if (node.isLiteral()) return;
+			if (node.isAnon()) return;
+			
+			String nodeURI = node.asNode().getURI();
+			logger.info("Validating " + nodeURI);
+			
+			if (urlValidator.isValid(nodeURI)) {
+				nodeTrailSet.add(node);
+			} else {
+				logger.info(node + " is a malformed URI");
+				bw.write(node.toString() + System.lineSeparator());
+			}
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		}
+	}
 
+	public void checkNodeURIStatus(RDFNode node, BufferedWriter bw) {
 		try {
 			String URI = node.asResource().getURI();
-			logger.info("Checking " + URI);
+			logger.info("Visiting " + URI);
 
 			Document doc = Jsoup.connect(URI)
 					.timeout(6000)
@@ -113,8 +147,6 @@ public class ResourceURIStatusChecker {
 			} catch (Exception e2) {
 				logger.error(e2.getMessage(), e2);
 			}
-		} finally {
-			nodeTrailSet.add(node);
 		}
 	}
 }
